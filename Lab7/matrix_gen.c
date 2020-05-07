6ff0f8136d0fd7007ff0a3ade91dbd16db777647
Gagan748 <gaganpreetmultani717@gmail.com>
2020-05-07 01:15:56 -0700

    to generate matrix(with some minor changes)

--- Lab7/matrix_gen.c
+++ Lab7/matrix_gen.c
@@ -5,9 +5,14 @@ int main(int argc , char *argv[])
 {
 int i,j,n;
 unsigned int upper, lower;
+if(argc<2)
+{
+fprintf)stderr,"Insufficient input");
+exit(EXIT_FAILURE);
+}
 upper=100;
 lower=-100;
-n=atoi(argv[1]);  //converting argv[1] to int
+n=atoi(argv[1]);  //converting argv[1] to int, size of matrix = nXn
 for(i=0;i<n;i++)
 {
 for(j=0;j<n;j++)
